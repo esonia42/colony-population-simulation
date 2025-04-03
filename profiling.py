35e0d9cc997983ecb9aaf5e8c0349ef952911169
esonia42 <soniarojkova42@gmail.com>
2025-04-02 22:50:01 -0300

    pool

--- profiling.py
+++ profiling.py
@@ -1,11 +1,11 @@
 from multiprocessing import Process
 
 import time
-from psycopg_db import get_one_person, get_conn, get_multiple_persons, loop_over_tasks
-
+from psycopg_db import DbInterface
 
 if __name__ == '__main__':
-    batch_size = 10000
+    db_interface = DbInterface()
+    batch_size = 1000
 
     # write a general multiprocessing func in order to test 2 workers/conns, 5 workers/cons, 50 w/c
     # increase test batch size to 5000
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     def make_workers(n):
         p_list = []
         for i in range(n):
-            p = Process(target=loop_over_tasks(int(batch_size/n)))
+            p = Process(target=db_interface.loop_over_tasks(number_of_tasks=int(batch_size/n)))
             p_list.append(p)
         return p_list
 
@@ -34,12 +34,11 @@ if __name__ == '__main__':
 
     test_n_workers(2)
     test_n_workers(5)
-    test_n_workers(10)
+    test_n_workers(50)
 
     t1 = time.perf_counter(), time.process_time()
-    new_conn = get_conn()
     for i in range(batch_size):
-        get_one_person(connection=new_conn)
+        db_interface.get_one_person()
 
     t2 = time.perf_counter(), time.process_time()
 
@@ -48,10 +47,10 @@ if __name__ == '__main__':
 
     t1 = time.perf_counter(), time.process_time()
 
-    get_multiple_persons(batch_size)
+    db_interface.get_multiple_persons(batch_size)
 
     t2 = time.perf_counter(), time.process_time()
 
     print(f"{batch_size} persons real time: {t2[0] - t1[0]:.2f} seconds")
     print(f"{batch_size} persons CPU time: {t2[1] - t1[1]:.2f} seconds")
-
+    db_interface.close_pool()
--- psycopg_db.py
+++ psycopg_db.py
@@ -1,60 +1,65 @@
 import psycopg
+from psycopg_pool import ConnectionPool
 
 
-conn = psycopg.connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10 sslmode=prefer")
-conn.commit()
+class DbInterface:
+    def __init__(self):
+        self.single_conn = self.get_conn()
+        self.pool = self.get_pool()
 
-def get_conn():
-    conn = psycopg.connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10 sslmode=prefer")
-    conn.commit()
-    return conn
+    def get_pool(self):
+        pool = ConnectionPool("host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10 sslmode=prefer")
+        return pool
 
-def insert_persons(person):
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO persons (person_id, sex, age, name, colony) VALUES (%s, %s, %s, %s, %s)",
-        (person.id, person.sex, person.age, person.name, person.colony.name))
-    conn.commit()
+    def get_conn(self):
+        conn = psycopg.connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10 sslmode=prefer")
+        conn.commit()
+        return conn
 
-def remove_persons(person):
-    cur = conn.cursor()
-    cur.execute(
-        "DELETE FROM orders WHERE person_id = person_id")
-    conn.commit()
+    def insert_persons(self, person):
+        cur = self.single_conn.cursor()
+        cur.execute(
+            "INSERT INTO persons (person_id, sex, age, name, colony) VALUES (%s, %s, %s, %s, %s)",
+            (person.id, person.sex, person.age, person.name, person.colony.name))
+        self.single_conn.commit()
 
-def get_persons():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM persons;")
-    cur.fetchall()
-    conn.commit()
+    def remove_persons(self, person):
+        cur = self.single_conn.cursor()
+        cur.execute(
+            "DELETE FROM persons WHERE person_id = %s", (person.id, ))
+        self.single_conn.commit()
 
-def delete():
-    cur = conn.cursor()
-    cur.execute("DELETE FROM persons;")
-    cur.fetchall()
-    conn.commit()
+    def get_persons(self):
+        cur = self.single_conn.cursor()
+        cur.execute("SELECT * FROM persons;")
+        cur.fetchall()
+        self.single_conn.commit()
 
+    def delete(self):
+        cur = self.single_conn.cursor()
+        cur.execute("DELETE FROM persons;")
+        cur.fetchall()
+        self.single_conn.commit()
 
-def get_one_person(connection):
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM persons;")
-    cur.fetchone()
-    conn.commit()
+    def get_one_person(self):
+        with self.pool.connection() as conn:
+            return conn.execute("SELECT * FROM persons;").fetchone()
 
+    def get_multiple_persons(self, n):
+        with self.pool.connection() as conn:
+            return conn.execute("SELECT * FROM persons;").fetchmany(n)
 
-def get_multiple_persons(n):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM persons;")
-    cur.fetchmany(n)
-    conn.commit()
+    def loop_over_tasks(self, number_of_tasks: int):
+        for i in range(number_of_tasks):
+            self.get_one_person()
+        print(number_of_tasks, " finished")
 
+    def close_pool(self):
+        self.pool.close()
 
-def loop_over_tasks(number_of_tasks: int):
-    connection = get_conn()
-    for i in range(number_of_tasks):
-        get_one_person(connection)
-    print(number_of_tasks, " finished")
+class MegaDbInterface(DbInterface):
+    def extra_function(self):
+        pass
 
-'''with conn.cursor() as cursor:
-    cursor.execute("EXPLAIN ANALYZE SELECT * FROM persons WHERE column = %s", ("person_id",))
-    print(cursor.fetchall())  # Check the query plan'''
+megadb = MegaDbInterface()
+megadb.get_multiple_persons()
